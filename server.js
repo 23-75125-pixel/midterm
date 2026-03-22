daf7968674c5dbe15b27e708971b5a9bf063b906
23-75125-pixel <23-75125@g.batstate-u.edu.ph>
2026-03-22 08:44:08 -0700

    a

--- server.js
+++ server.js
@@ -6,7 +6,11 @@ const path = require('path');
 const app = express();
 const server = http.createServer(app);
 const io = new Server(server, {
-  cors: { origin: '*' }
+  cors: { origin: '*', methods: ['GET', 'POST'] },
+  transports: ['websocket'],
+  allowUpgrades: false,
+  pingTimeout: 60000,
+  pingInterval: 25000
 });
 
 app.use(express.static(path.join(__dirname, 'public')));
